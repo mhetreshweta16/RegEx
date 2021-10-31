0aa5f14f30eb6b04b9d51a4d6f5e89974d61102b
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-31 09:02:50 +0530

    [shweta] added the hello

---
+++ RegEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
